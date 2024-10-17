e8791e268d67ad17c6bc77cb94e7ff4eafd89e8b
Prem <prem.gujel@ibytinfomatics.in>
2024-10-17 11:35:12 +0530

    update App file

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import { Routes,Route } from 'react-router-dom';
 import NewProduct from './modules/Products/NewProduct.js';
 import AllProduct from './component/all product/AllProduct.js';
 import Cart from './modules/Cart.js';
-import Contact from './component/Contact.js';
 import Addtocard from './component/Addtocard.js';
 import About from './component/About.js';
 import Login from './component/Login.js';
+import Cleaner from './component/Cleaner.js';
 function App() {
   return (
     <>
@@ -19,10 +19,10 @@ function App() {
         <Route path = "/allproduct/:id" element = {<NewProduct/>} />
         <Route path ="/allproduct" element ={<AllProduct/>}/>
         <Route path ="/cart" element = {<Cart />} />
-        <Route path = "/contact" element = {<Contact />} />
         <Route path = "/addtocard/:id" element = {<Addtocard/>} />
         <Route path = "/about" element = {<About />} />
         <Route path = "/login" element ={<Login />} />
+        <Route path ="cleaning" element ={<Cleaner/>} />
         {/* <Route path = "header" element={<Header/>} /> */}
         </Routes> 
       </div>
